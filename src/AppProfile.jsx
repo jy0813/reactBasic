e5b4a49e418b04d6a281dd32a94f535ab9fb9438
jy0813 <busbug3@gmail.com>
2023-01-26 11:20:07 +0900

    feat: props 사용

--- src/AppProfile.jsx
+++ src/AppProfile.jsx
@@ -4,7 +4,21 @@ import Profile from './components/Profile';
 function AppProfile() {
   return (
     <>
-      <Profile />
+      <Profile
+        image='https://plus.unsplash.com/premium_photo-1672281090684-06f42ed9827f?ixlib=rb-4.0.3&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=709&q=80'
+        name='James Kim'
+        title='Frontend Engineer'
+      />
+      <Profile
+        image='https://images.unsplash.com/photo-1674644949503-b77f20451871?ixlib=rb-4.0.3&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=694&q=80'
+        name='Anna Young'
+        title='Frontend Engineer'
+      />
+      <Profile
+        image='https://images.unsplash.com/photo-1674594430972-c3b9e6589e53?ixlib=rb-4.0.3&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=687&q=80'
+        name='Bob Yu'
+        title='Backend Engineer'
+      />
     </>
   );
 }
--- src/components/Profile.jsx
+++ src/components/Profile.jsx
@@ -1,15 +1,11 @@
 import React from 'react';
 
-export default function Profile() {
+export default function Profile({ image, name, title }) {
   return (
     <div className='profile'>
-      <img
-        className='photo'
-        src='https://images.unsplash.com/photo-1674641672189-f3de2f2cfc31?ixlib=rb-4.0.3&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=1470&q=80'
-        alt='avatar'
-      />
-      <h1>James Kim</h1>
-      <p>Frontend Engineer</p>
+      <img className='photo' src={image} alt='avatar' />
+      <h1>{name}</h1>
+      <p>{title}</p>
     </div>
   );
 }

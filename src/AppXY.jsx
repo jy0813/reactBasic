7a06afe63a09f2208d3d33b18b392b879b944f12
jy0813 <busbug3@gmail.com>
2023-01-27 14:43:38 +0900

    feat: useState 객체변환

--- src/AppXY.jsx
+++ src/AppXY.jsx
@@ -2,21 +2,18 @@ import React, { useState } from 'react';
 import './AppXY.css';
 
 export default function AppXY() {
-  const [mouseX, setMouseX] = useState(0);
-  const [mouseY, setMouseY] = useState(0);
+  const [position, setPosition] = useState({ x: 0, y: 0 });
 
   return (
     <div
       className='container'
       onPointerMove={(e) => {
-        console.log(e.clientX, e.clientY);
-        setMouseX(e.clientX);
-        setMouseY(e.clientY);
+        setPosition({ x: e.clientX, y: e.clientY });
       }}
     >
       <div
         className='pointer'
-        style={{ transform: `translate(${mouseX}px, ${mouseY}px)` }}
+        style={{ transform: `translate(${position.x}px, ${position.y}px)` }}
       />
     </div>
   );
